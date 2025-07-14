726fc495c20c8c98e240ecff8c58d40d0d27097b
SK-Sarkar-Programmer <shawonkumarsarkar.developer@gmail.com>
2025-07-14 20:05:07 +0600

    Three title added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log(10)
+document.addEventListener("DOMContentLoaded", function () {
+  // Banner Height Measurement
+  const bannerHeightCalc = document.querySelector("#banner-height-calc");
+  const bannerLastDiv = document.querySelector("#banner-last-div");
+  bannerHeightCalc.style.height = `calc(98% - ${bannerLastDiv.offsetHeight}px)`;
+
+  // Navigation Menu Toggle
+  const menuBtns = document.querySelectorAll(".menu-btn");
+  let isMenuOpen = false;
+
+  menuBtns.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      isMenuOpen = !isMenuOpen;
+    });
+  });
+});
