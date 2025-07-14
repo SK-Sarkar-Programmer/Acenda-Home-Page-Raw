16a7f6c3a0127c3b09bd499371850d2016997a6d
SK-Sarkar-Programmer <shawonkumarsarkar.developer@gmail.com>
2025-07-15 00:02:29 +0600

    Why choose us section responsive complete

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Banner Height Measurement
-  const bannerHeightCalc = document.querySelector("#banner-height-calc");
-  const bannerLastDiv = document.querySelector("#banner-last-div");
-  bannerHeightCalc.style.height = `calc(98% - ${bannerLastDiv.offsetHeight}px)`;
-
   // Navigation Menu Toggle
   const menuBtns = document.querySelectorAll(".menu-btn");
   let isMenuOpen = false;
@@ -13,4 +8,25 @@ document.addEventListener("DOMContentLoaded", function () {
       isMenuOpen = !isMenuOpen;
     });
   });
+
+  // Banner Height Measurement
+  const bannerHeightCalc = document.querySelector("#banner-height-calc");
+  const bannerLastDiv = document.querySelector("#banner-last-div");
+  bannerHeightCalc.style.height = `calc(98% - ${bannerLastDiv.offsetHeight}px)`;
+
+  // Partners Logo Marquee
+  if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    const partnersMarquee = document.querySelector("#partners-marquee");
+    partnersMarquee.setAttribute("marquee-animation", true);
+
+    const logoSlides = document.querySelector("#logo-slides");
+    const logoSlidesCopies = Array.from(logoSlides.children);
+
+    logoSlidesCopies.forEach((child) => {
+      const logoCopied = child.cloneNode(true);
+      logoCopied.setAttribute("aria-hidden", true);
+
+      logoSlides.appendChild(logoCopied);
+    });
+  }
 });
